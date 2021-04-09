c639606a1b9d283a3f5f3cb59554ac631d970aff
Jok3r182 <71766496+Jok3r182@users.noreply.github.com>
2021-04-09 17:58:12 +0300

    Initial commit

---
+++ Back-End/Database/Database.py
@@ -0,0 +1,33 @@
+from peewee import *
+
+user = 'root'
+password = ''
+db_name = 'fast_api_emotion_detection'
+
+db = MySQLDatabase(db_name, user=user, password=password, host='localhost')
+
+
+class User(Model):
+    username = CharField()
+    password = CharField()
+    email = CharField()
+
+    class Meta:
+        database = db
+
+
+class Emotions(Model):
+    happy = FloatField()
+    angry = FloatField()
+    disgust = FloatField()
+    neutral = FloatField()
+    sad = FloatField()
+    surprised = FloatField()
+    feared = FloatField()
+
+    class Meta:
+        database = db
+
+
+db.connect()
+db.create_tables([User, Emotions])
---
+++ Back-End/Emotion_Detection/Emotion_Detection.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+import tensorflow as tf
+import os
+
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+new_model = tf.keras.models.load_model("../Trained_Models/emotionDetection_InceptionV3.h5")
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+def checkPictureEmotion(path, size):
+    frame = cv2.imread(path)
+    grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(grayImg, 1.1, 4)
+    for x, y, w, h in faces:
+        roi_gray = grayImg[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        facess = faceCascade.detectMultiScale(roi_gray)
+        if len(facess) == 0:
+            print("Veidas nebuvo aptiktas")
+        else:
+            for (ex, ey, ew, eh) in facess:
+                face_roi = roi_color[ey:ey + eh, ex:ex + ew]
+    final_image = cv2.resize(face_roi, (size, size))
+    final_image = np.expand_dims(final_image, axis=0)
+    final_image = final_image / 255.0
+    predictions = new_model.predict(final_image)
+
+    if (np.argmax(predictions) == 0):
+        status = "Išsigandęs"
+    elif (np.argmax(predictions) == 1):
+        status = "Linksmas"
+    elif (np.argmax(predictions) == 2):
+        status = "Liūdnas"
+    elif (np.argmax(predictions) == 3):
+        status = "Neutralus"
+    elif (np.argmax(predictions) == 4):
+        status = "Nustebęs"
+    elif (np.argmax(predictions) == 5):
+        status = "Pasišlykštėjęs"
+    elif (np.argmax(predictions) == 6):
+        status = "Piktas"
+    print(status)
+    x = np.argmax(predictions)
+    print(predictions[0][x] * 100)
+
+
+checkPictureEmotion("../Emotion_Images/surprised1.jpg", 299)

4ce7a817beba7e5daff1bc7f9cbccfb178fd5f50
Gwhan <justinasken@gmail.com>
2021-04-11 15:49:18 +0300

    alot of changes

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,20 +1,32 @@
 from peewee import *
 
 
-class User(Model):
-    username = CharField()
-    password = CharField()
-    email = CharField()
+class DB:
+    db = None
+    def __init__(self, user, password, db_name, host):
+        self.user = user
+        self.password = password
+        self.db_name = db_name
+        self.host = host
+        self.db = MySQLDatabase(db_name, user=user, password=password, host=host)
 
-    class Meta:
-        database = db
+    def start(self):
+        self.db.connect()
+        self.db.create_tables([User, Emotions])
+
+class User(Model):
+        username = CharField()
+        password = CharField()
+        email = CharField()
+        class Meta:
+            database = DB.db
 
 class Images(Model):
     emotionID = IntegerField()
     userID = IntegerField()
     image = Field
     class Meta:
-        database=db
+        database = DB.db
 
 class Emotions(Model):
     happy = FloatField()
@@ -24,20 +36,7 @@ class Emotions(Model):
     sad = FloatField()
     surprised = FloatField()
     feared = FloatField()
-
     class Meta:
+        database = DB.db
 
-        database = Database.db
-
-class Database:
-    def __init__(self, user, password, db_name, host):
-        self.user = user
-        self.password = password
-        self.db_name = db_name
-        self.host = host
-        self.db = MySQLDatabase(db_name, user=user, password=password, host=host)
-        
-    def start(self):
-        self.db.connect()
-        self.db.create_tables([User, Emotions])
 

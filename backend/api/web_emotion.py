31dc240be10e64ce1b795f5975a239a6a105df3f
Gwhan <justinasken@gmail.com>
2021-04-11 15:08:19 +0300

    changed db

--- backend/api/web_emotion.py
+++ backend/api/web_emotion.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, UploadFile
 import requests
 
 app = FastAPI()
+db = database.Database('root', "", 'fast_api_emotion_detection', '78.31.188.217')
+db.start()
 
 @app.get('/')
 def index():

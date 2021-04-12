7620e9a7e93d15b3be352b72c3a488a1216537db
Gwhan <justinasken@gmail.com>
2021-04-12 12:52:54 +0300

    Changed stuff

--- run_api.py
+++ run_api.py
@@ -1,11 +1,23 @@
+from re import template
 from fastapi import FastAPI, UploadFile
-from backend.db.database import DB
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.requests import Request
+from core.db.database import DB
 import requests
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="static/templates")
+
 db = DB.getFirstInstance('root', "", 'fast_api_emotion_detection', '78.31.188.217')
 db.start()
 
 @app.get('/')
-def index():
-    return "sveiki"
+def index(request: Request):
+    return templates.TemplateResponse("login.html", {"request":request})
+
+@app.get('/register')
+def index(request: Request):
+    return templates.TemplateResponse("register.html", {"request":request})
